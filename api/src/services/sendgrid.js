76cd430138ed9b23bd253e422da103805e36a73e
gon salinas <gonzalosalinasss.96@gmail.com>
2022-02-08 23:21:56 -0300

    fix variable email sendgrid

--- api/src/services/sendgrid.js
+++ api/src/services/sendgrid.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const {API_KEY} = process.env;
+const {API_KEY,EMAIL_SENDGRID} = process.env;
 const sgMail= require('@sendgrid/mail');
  
 sgMail.setApiKey(API_KEY);
@@ -7,7 +7,7 @@ sgMail.setApiKey(API_KEY);
 async function sendEmail (email){
   const msg = {
     to: `${email}`, // Change to your recipient
-    from: 'gonzalosalinasss.96@gmail.com', // Change to your verified sender
+    from: `${EMAIL_SENDGRID}`, // Change to your verified sender
     subject: 'Enviado con SendGrid desde api challenge-backend-alkemy',
     text: 'Bienvenido a la platafarma challenge-backend alkemy',
     html: '<strong>Bienvenido a la platafarma challenge-backend alkemy</strong>',
